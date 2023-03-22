3588d4209c0c7acaf0b3f3982f8a61da94a77313
Finn, Ryan <Ryan.Finn@mines.sdsmt.edu>
2023-03-22 14:45:39 -0600

    ...

--- PolyRegression.py
+++ PolyRegression.py
@@ -1,46 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LinearRegression
 
 np.random.seed(42)
+d = 2
 
-# Create 100 random points 0<X<2, y=3*x+4 +random noise
+# Create 100 random points 0<X<2, y = -2X2 + 3X + 4 + random noise
 m = 100
 X = 2 * np.random.rand(m, 1)
-y = 4 + 3 * X + np.random.randn(m, 1)  #
+y = -2 * X * X + 3 * X + 4 + np.random.randn(m, 1)
+print('X:', *X)
 
-plt.plot(X, y, "b.")
-plt.xlabel("$x_1$", fontsize=18)
-plt.ylabel("$y$", rotation=0, fontsize=18)
-plt.axis([0, 2, 0, 15])
-plt.show()
+c = [X, np.ones((m, 1))]
+c.insert(0, c[0] * X)
+print(*np.c_[c])
+X_b = np.c_[X * X, X, np.ones((m, 1))]  # add x1 = 1 to each instance
+print('X_b:', *X_b)
+A_inv = np.linalg.inv(X_b.T.dot(X_b))
+print('A Inverse:', *A_inv)
+b = X_b.T.dot(y)
+print('b:', *b)
+w_best = A_inv.dot(b)
+print('Best w:', *w_best)
 
-print(X)
-print(np.c_[X, np.ones((m, 1))])
+print('w (using psuedo-inv):', *np.linalg.pinv(X_b).dot(y))
 
-X_b = np.c_[X, np.ones((m, 1))]  # add x1 = 1 to each instance
-w_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
-print(w_best)
-
-A = np.linalg.inv(X_b.T.dot(X_b))
-print(A)
-
-X_new = np.array([[0], [2]])
-X_new_b = np.c_[X_new, np.ones((2, 1))]  # add x1 = 1 to each instance
-y_predict = X_new_b.dot(w_best)
-print(y_predict)
-
-plt.plot(X_new, y_predict, "r-", linewidth=2, label="Predictions")
+fig1 = plt.figure()
+x_curve = np.linspace(0, 2, 100)
+plt.plot(x_curve, [np.polyval(w_best, i) for i in x_curve], "r-", linewidth=2, label="Predictions")
 plt.plot(X, y, "b.")
 plt.xlabel("$x_1$", fontsize=18)
 plt.ylabel("$y$", rotation=0, fontsize=18)
 plt.legend(loc="upper left", fontsize=14)
-plt.axis([0, 2, 0, 15])
+plt.xlim([0, 2])
 plt.show()
-
-print(np.linalg.pinv(X_b).dot(y))
-
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
-print(lin_reg.intercept_, lin_reg.coef_)
-print(lin_reg.predict(X_new))
